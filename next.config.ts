f0583d2c6bd0f14e62f58cc348f1f53568c7afe7
hismanhs <hismanhs@gmail.com>
2025-01-21 19:04:53 +0530

    MFE config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js for Dashboard (Microfrontend)
+const { ModuleFederationPlugin } = require('webpack').container;
+module.exports = {
+  webpack: (config:any , { isServer }:any) => {
+    // Apply Module Federation only for the client-side
+    if (!isServer) {
+      config.plugins.push(
+        new ModuleFederationPlugin({
+          name: 'dashboard',
+          filename: 'remoteEntry.js',
+          exposes: {
+            './Dashboard': './pages/index.js', // Exposing the Dashboard component
+          },
+          shared: ['react', 'react-dom'], // Sharing common dependencies
+        })
+      );
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
